73897920b619ccf85797601f05370a9a79bc71ea
Ojas Baghele <95533514+ojasbaghele@users.noreply.github.com>
2023-10-26 13:13:28 +0530

    Textutil-v1.0

---
+++ Textutil.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Textutil.css';
+
+export default function Textutil() {
+  return (
+    <div>
+    <div id="input_group">
+        <h1>Text Analizer</h1>
+        <textarea class="form-control" aria-label="With textarea"></textarea>
+        <div id="btn_group">
+        <button type="button" id="btns"class="btn btn-outline-primary">Primary</button>
+        <button type="button" id="btns"class="btn btn-outline-primary">Primary</button>
+        <button type="button" id="btns"class="btn btn-outline-primary">Primary</button>
+        <button type="button" id="btns"class="btn btn-outline-primary">Primary</button>
+        </div>
+    </div>
+    
+    </div>
+  )
+}
