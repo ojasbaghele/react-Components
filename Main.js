3e0a6a87a95a5b2b6ac98969772b7dd67f23f695
Ojas Baghele <95533514+ojasbaghele@users.noreply.github.com>
2023-11-03 19:36:15 +0530

    React Layout Divided in Components-v_1.0
    page layout is divided into different different  components like Header, Footer, Sidebar, Navbar.

---
+++ Main.js
@@ -0,0 +1,13 @@
+import { React } from 'react';
+function Main (){
+    return ( 
+        <div class="text-area">
+            <h1>This is Content Area</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe non earum possimus, cumque delectus fuga, facilis magni velit similique accusantium eaque quia facere sint exercitationem voluptate quis et. Magni,
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus voluptatum eos recusandae sit totam nesciunt saepe distinctio ducimus, labore cum quis vero nulla voluptas odio qui veniam, facilis eaque? Eaque 
+            tempore perferendis expedita dolorum adipisci? Animi corporis at ab quas minus aperiam, molestiae placeat omnis possimus deserunt exercitationem numquam est!</p>
+        </div>
+
+    )
+}
+export default Main;
---
+++ SidebarLeft.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Sidebarleft() {
+  return (
+    <div>
+    <h3> Sidebar Left</h3>
+
+      <p> Lable 1 </p>
+      <p> Lable 2 </p>
+      <p> Lable 3 </p>
+      <p> Lable 4 </p>
+      <p> Lable 5 </p>
+      <p> Lable 6 </p>
+      <p> Lable 7 </p>
+
+    </div>
+  )}
